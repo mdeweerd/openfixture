30a64046445ab0c551c224068b695f6ab44b1d00
Eric Poulsen <eric@zyxod.com>
2017-12-12 09:20:24 -0800

    auto-converts logo from svg to dxf, reads size for use by openscad

--- genfixture.sh
+++ genfixture.sh
@@ -5,18 +5,18 @@
 #
 
 BOARD=$1
-OUTPUT="fixture-v10"
+OUTPUT="rev1"
 
 # PCB thickness
-PCB=0.8
-LAYER='B.Cu'
-REV='rev.10'
+PCB=1.6
+LAYER='F.Cu'
+REV='rev1'
 
 # Nearest opposite side component to border
 BORDER=0.8
 
 # Material dimensions
-MAT=2.45
+MAT=3.0
 SCREW_LEN=16.0
 SCREW_D=3.0
 WASHER_TH=1.0
@@ -24,6 +24,21 @@ NUT_TH=2.4
 NUT_F2F=5.45
 NUT_C2C=6.10
 
+# convert logo
+rm logo.dxf
+inkscape logo.svg -E logo.eps
+pstoedit -dt -f "dxf:-polyaslines -mm" logo.eps logo.dxf
+rm logo.eps
+
+
+LOGO_WIDTH=$(echo "scale = 4; $(inkscape logo.svg -W 2>/dev/null) / 90.0 * 25.4" | bc)
+LOGO_HEIGHT=$(echo "scale = 4; $(inkscape logo.svg -H 2>/dev/null) / 90.0 * 25.4" | bc)
+
+echo $LOGO_WIDTH x $LOGO_HEIGHT
+
+
+
+
 # Call python wrapper
-python GenFixture.py --board $BOARD --layer $LAYER --rev $REV --mat_th $MAT --pcb_th $PCB --out $OUTPUT --screw_len $SCREW_LEN --screw_d $SCREW_D --washer_th $WASHER_TH --nut_th $NUT_TH --nut_f2f $NUT_F2F --nut_c2c $NUT_C2C --border $BORDER
+python GenFixture.py --board $BOARD --layer $LAYER --rev $REV --mat_th $MAT --pcb_th $PCB --out $OUTPUT --screw_len $SCREW_LEN --screw_d $SCREW_D --washer_th $WASHER_TH --nut_th $NUT_TH --nut_f2f $NUT_F2F --nut_c2c $NUT_C2C --border $BORDER --logo-w $LOGO_WIDTH --logo-h $LOGO_HEIGHT
 
